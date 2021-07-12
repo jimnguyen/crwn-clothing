c551d686154eda0090c4f4b7ae6c2c5376e2da0a
Jim Nguyen <vjimnguyen@gmail.com>
2021-07-11 20:17:17 -0700

    Finished home page outline

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
 
 export default App;
